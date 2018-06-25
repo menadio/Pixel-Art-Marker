8b6d840ffb9d59779f5ef12aa931dcad611b11ca
Elomena Idise <menadio1@gmail.com>
2018-06-26 00:54:31 +0100

    Commence code change tracking

---
+++ main.js
@@ -0,0 +1,44 @@
+// Get the user selected color
+function selectedColor() {
+    const color = $('#selectedColor').val();
+    return color;
+}
+
+// Fill function
+function fillBox(element) {
+    // let selectedColor = 'orange';
+    $(element).css('background-color', selectedColor());
+}
+
+$(document).ready(function() {
+    // Create the grid canvas for art
+    function makeGrid() {
+        let numOfRow = 0; let numOfCol = 0;
+
+        $('#submit').on('click', function() {
+
+            numOfRow = $('#height').val();
+            numOfCol = $('#width').val();
+            const body = $('.grid-canvas');
+
+            for (var i = 1; i <= numOfRow; i++) {
+                let row = $('<tr id=\'' + i + '\'></tr>');
+                
+                for (col = 1; col <= numOfCol; col++) {
+                    // row.append('<td id=\'' + i + '-' + col + '\'></td>');
+                    row.append('<td onclick="fillBox(this)" id=\'r' + i + 'c' + + col + '\'></td>');
+                }
+
+                body.append(row);
+            }
+        });
+    }
+
+    makeGrid();
+
+    // Reset design canvas grid
+    $('#reset').click(function() {
+        let tr = $('tbody').children('tr');
+        tr.remove();
+    });
+})
